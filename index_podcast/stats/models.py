69b134e03eff4e10f48d0588432f5bdf258187ce
Mikhail <misha.beliy15@gmail.com>
2019-11-29 04:06:48 +0200

    Added viewsEpisodes

--- index_podcast/stats/models.py
+++ index_podcast/stats/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 
 class ViewsPodcast(models.Model):
-    podcast = models.ForeignKey('mypodcasts.Podcast', on_delete=models.CASCADE)
+    podcast = models.ForeignKey('mypodcasts.Podcast', related_name="views", on_delete=models.CASCADE)
     datetime = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"[{self.datetime}] {self.podcast.title}"
 
+
+class ViewsEpisode(models.Model):
+    episode = models.ForeignKey('mypodcasts.Episode', related_name="views", on_delete=models.CASCADE)
+    datetime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"[{self.datetime}] {self.episode.title}"
